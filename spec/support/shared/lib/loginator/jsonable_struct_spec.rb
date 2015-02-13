62f3e3063deee1168687b656ab20a7639f8743a2
Greg Poirier <grep@binary-snobbery.com>
2015-02-13 15:49:39 -0800

    Add type to json body for easier serialization/deserialization

--- spec/support/shared/lib/loginator/jsonable_struct_spec.rb
+++ spec/support/shared/lib/loginator/jsonable_struct_spec.rb
@@ -1,13 +1,13 @@
 shared_examples_for 'jsonable_struct' do
   describe '#to_json' do
     it 'faithfully serializes' do
-      expect(subject.to_json).to eq(MultiJson.dump(subject.to_h))
+      expect(subject.to_json).to eq(MultiJson.dump(subject.to_h.merge(type: described_class.type)))
     end
   end
 
-  describe '.from_hash' do
+  describe '.from_json' do
     it 'faithfully deserializes' do
-      expect(described_class.from_hash(MultiJson.load(subject.to_json))).to eq(subject)
+      expect(described_class.from_json(MultiJson.load(subject.to_json))).to eq(subject)
     end
   end
 end
